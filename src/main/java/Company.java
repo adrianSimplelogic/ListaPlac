6b5a358981785106ae970bd3fac6e9c7d6ae24ea
Adrian <a.szymanski@simplelogic.pl>
2022-12-05 00:44:24 +0100

    commit

--- src/main/java/Company.java
+++ src/main/java/Company.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class Company {
+import java.util.List;
+
+public class Company {
+
+    private List<Employee> employeeList;
+
+    public Company(List<Employee> employeeList) {
+        this.employeeList = employeeList;
+    }
+
+    public List<Employee> getEmployeeList() {
+        return employeeList;
+    }
+
+    public void setEmployeeList(List<Employee> employeeList) {
+        this.employeeList = employeeList;
+    }
+
+    public void addEmployee(Employee employee) {
+        employeeList.add(employee);
+    }
+
+    public void printEmployees() {
+        for (Employee employee : employeeList) {
+            System.out.println(employee.toString());
+//            employee.printInfo();
+        }
+    }
+
+    public int getSum() {
+        int inTotal = 0;
+        for (Employee employee : employeeList) {
+            inTotal += employee.getSalary();
+        }
+        return inTotal;
+    }
+
+    public int getAvr() {
+        return getSum()/employeeList.size();
+    }
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Main {
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Employee employee01 = new Employee("Adam","Boski",2000);
+        Employee employee02 = new Employee("Ewa","Rajska",2200);
+        Employee employee03 = new Employee("Robert","Lewandowski",2400);
+        Employee employee04 = new Employee("Wojciech","Nieszczęsny",2600);
+        Employee employee05 = new Employee("Beata","Krawiec",2800);
+
+        Company company = new Company(new ArrayList<>());
+        company.addEmployee(employee01);
+        company.addEmployee(employee02);
+        company.addEmployee(employee03);
+        company.addEmployee(employee04);
+        company.addEmployee(employee05);
+
+        company.printEmployees();
+        System.out.println("Total salary: "+company.getSum());
+        System.out.println("Average salary: "+company.getAvr());
+    }
+
 }
